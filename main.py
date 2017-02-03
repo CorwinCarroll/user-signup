9d2abfd868079eea22c304ce0c3ce5a99e355420
CorwinCarroll <corwin.p.carroll@gmail.com>
2017-02-02 19:21:43 -0600

    kind of works, does not retain usernames or email

--- main.py
+++ main.py
@@ -12,41 +12,42 @@ page_footer = """
 </html>
 """
 signup_form = """
-<h1><a href="/">User Sign Up</a></h1>
-<form action="/welcome" method="post" >
-    <table>
-        <tr>
-            <td><label for="username"> Username:</label> </td>
-            <td><input name="username" type="text" value="{0}"></td>
-            <td style="color: red;"> {1} </td>
-        </tr>
-        
-        <tr>
-            <td><label for="password"> Password: </label></td>
-            <td><input name="password" type="password"> </td>
-            <td style="color: red;"> {2} </td>
-        </tr>
-        
-        <tr>
-            <td><label for="valid_pw"> Confirm Password:</label></td>
-            <td><input name="valid_pw" type="password"> </td>
-            <td style="color: red;"> {3} </td>
-        </tr>
-        
-        <tr>
-            <td><label for="email"> Email (optional):</label></td>
-            <td><input name="email" type="text"> </td>
-            <td style="color: red;"> {4} </td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="submit" value="Sign Up!"/></td>
-        </tr>
-    </table>
-</form>
+<fieldset>
+    <legend><a href="/">User Sign Up</a></legend>
+    <form action="/welcome" method="post" autocomplete="on">
+        <table>
+            <tr>
+                <td><label for="username"> Username:</label> </td>
+                <td><input name="username" type="text" {}></td>
+                <td style="color: red;"> {}</td>
+            </tr>
+            
+            <tr>
+                <td><label for="password"> Password: </label></td>
+                <td><input name="password" type="password"> </td>
+                <td style="color: red;"> {} </td>
+            </tr>
+            
+            <tr>
+                <td><label for="valid_pw"> Confirm Password:</label></td>
+                <td><input name="valid_pw" type="password"> </td>
+                <td style="color: red;"> {} </td>
+            </tr>
+            
+            <tr>
+                <td><label for="email"> Email (optional):</label></td>
+                <td><input name="email" type="text"> </td>
+                <td style="color: red;"> {} </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="Sign Up!"/> <input type="reset" value="Reset" /></td>
+
+            </tr>
+        </table>
+    </form>
+</fieldset>
 """
-
-
 def valid_username(username):
     valid = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
     return valid.match(username)
@@ -59,12 +60,22 @@ def valid_password(password):
     valid = re.compile(r"^.{3,20}$")
     return valid.match(password)
 
-class Index(webapp2.RequestHandler):
-    ''' handles requests for main index page '''
+class MainHandler(webapp2.RequestHandler):
+    """ handles index page request """
+
+    # def write_form(self, (un_error, pw_error, vaildpw_error, email_error)=('','','','')):
+    #     self.response.write(page_header + signup_form.format(un_error, pw_error, vaildpw_error, email_error) 
+    #     + page_footer)
+        
+
     def get(self):
-        un_error = self.request.get('un_error')
+        # self.write_form()
+
         username = self.request.get('username')
-        un_error_element = username + '<p class="error">' + un_error + "</p>" if un_error else ""
+        saved_user = 'value="' + username + '"'
+
+        un_error = self.request.get('un_error')
+        un_error_element = '<p class="error">' + un_error + "</p>" if un_error else ""
 
         
         pw_error = self.request.get('pw_error')
@@ -76,17 +87,18 @@ class Index(webapp2.RequestHandler):
         email_error = self.request.get('email_error')
         email_error_element = '<p class="error">' + email_error + "</p>" if email_error else ""
         
-        self.response.write(page_header + signup_form.format(username, un_error, pw_error, vaildpw_error, email_error) \
-                            + page_footer)
+        self.response.write(page_header + signup_form.format(saved_user, un_error, pw_error, vaildpw_error, email_error) 
+        + page_footer)
 
 class Welcome(webapp2.RequestHandler):
-    ''' handles requests for welcome success page '''
+    """ handles welcome page request """
     def post(self):
         error = ""
         username = self.request.get('username')
         password = self.request.get('password')
         valid_pw = self.request.get('valid_pw')
         email = self.request.get('email')
+        saved_user = 'value="' + username + '"'
         
         if not valid_username(username):
             error += "un_error=Invalid Username"
@@ -114,11 +126,12 @@ class Welcome(webapp2.RequestHandler):
         
         if error != "":
             self.redirect("/?" + error)
-        
+            saved_user = 'value="' + username + '"'
+ 
         self.response.write(page_header + "Welcome to the other side, {}".format(username) + "!" + page_footer)
               
 
 app = webapp2.WSGIApplication([
-    ('/', Index),
+    ('/', MainHandler),
     ('/welcome', Welcome)
 ], debug=True)
